65d2fb79a814a441e04ff4c91fd5a33404762b32
Ziyan <jaraidaniel@gmail.com>
2015-04-21 15:07:13 +0200

    libstagefright: wfd: don't use intra macroblock refresh mode on omap4
    Most, if not all OMAP4 Ducatis doesn't support intra macroblock refresh mode,
    causing the encoder to fail initializing.
    This patch disables intra macroblock refresh mode for wifi display on omap4.
    
    Note:
    Ideally, the decoder shouldn't fail if intra macroblock refresh mode can't be
    configured. However, that would trick higher layers into thinking that it's on,
    because they set that parameter.
    As of now, this mode seems to only ever be used for wifi display.

--- media/libstagefright/wifi-display/Android.mk
+++ media/libstagefright/wifi-display/Android.mk
@@ -35,6 +35,10 @@ LOCAL_CFLAGS += -Wno-multichar -Werror -Wall
 LOCAL_CLANG := true
 LOCAL_SANITIZE := signed-integer-overflow
 
+ifeq ($(TARGET_BOARD_PLATFORM),omap4)
+LOCAL_CFLAGS += -DBOARD_NO_INTRA_MACROBLOCK_MODE_SUPPORT
+endif
+
 LOCAL_MODULE:= libstagefright_wfd
 
 LOCAL_MODULE_TAGS:= optional
